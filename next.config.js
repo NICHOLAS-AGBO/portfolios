28586b193259e6e1a7065c80589a53679846e2c4
NICHOLAS-AGBO <ndcconcept@gmail.com>
2023-04-10 02:47:23 +0100

    #Bugs fixed

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
-<<<<<<< HEAD
-=======
-    typedRoutes: true
-  },
-  images:{
-    dangerouslyAllowSVG: true
->>>>>>> 73b91e0c617e3bc18ace378cc6b8285217bf21ec
   }
 };
 
